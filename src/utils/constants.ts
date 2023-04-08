1e870046bdb8376cda90313ebab306121c12e651
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-08 15:32:27 +0300

    feature: establish prompts and constants for use in cli

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,5 @@ import pkg from '../../package.json'
 export const COMMAND_NAME = pkg.name
 export const REPO_URL = pkg.repository.url
 export const VERSION = pkg.version
+
+export const PROJECT_LIST = ['react', 'next', 'vue', 'nuxt']
