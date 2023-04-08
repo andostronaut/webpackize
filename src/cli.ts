a9e25bf3fcbfb9df4882d8594920bfa12bd19d36
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-08 08:40:49 +0300

    feature: setup cli and prompts

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,23 @@
-console.log('Generating config')
+import { cli } from 'cleye'
+import { COMMAND_NAME, VERSION } from './helpers/constants'
+import { getConfig } from './prompts'
+
+cli(
+  {
+    name: COMMAND_NAME,
+    version: VERSION,
+    flags: {
+      project: {
+        type: String,
+        description: 'Project type (React, Vue, etc...)',
+        alias: 'p',
+        default: '',
+      },
+    },
+  },
+  argv => {
+    const promptText = argv._.join(' ')
+
+    getConfig({ prompt: promptText })
+  }
+)
