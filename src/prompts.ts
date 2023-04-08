2bb2b8868f5837eda9d7d68e7156c21181047c39
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-08 17:25:26 +0300

    feature: re-struct prompts and create log utils

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,39 +1,21 @@
 import * as p from '@clack/prompts'
-import { setTimeout as sleep } from 'node:timers/promises'
 import color from 'picocolors'
 import _ from 'lodash'
+import { execaCommand } from 'execa'
+import { setTimeout as sleep } from 'node:timers/promises'
+import { copyFile } from 'node:fs/promises'
+
 import { PROJECT_LIST, CANCELED_OP_MSG } from './utils/constants'
 
 export const prompts = async ({ prompt }: { prompt?: string }) => {
   const promptLowercase = prompt?.toLowerCase() || ''
 
+  p.intro(color.blue('Generate webpack.config.js'))
+
   if (_.isUndefined(prompt) || _.isEmpty(prompt)) {
-    p.intro(color.blue('Generate webpack.config.js'))
+    const { projectType } = await getPromptProjectType()
 
-    const projectType = await p.select({
-      message: 'Pick a project type.',
-      options: [
-        { value: 'react', label: 'React' },
-        { value: 'vue', label: 'Vue' },
-        { value: 'next', label: 'Next' },
-        { value: 'nuxt', label: 'Nuxt' },
-      ],
-    })
-
-    if (p.isCancel(projectType)) {
-      p.cancel(CANCELED_OP_MSG)
-      return process.exit(0)
-    }
-
-    const installDeps = await p.confirm({
-      message: 'Install needed depedencies ?',
-      initialValue: true,
-    })
-
-    if (p.isCancel(installDeps)) {
-      p.cancel(CANCELED_OP_MSG)
-      return process.exit(0)
-    }
+    const { installDeps } = await getPromptInstallDeps()
 
     console.log({
       projectType,
@@ -41,6 +23,44 @@ export const prompts = async ({ prompt }: { prompt?: string }) => {
     })
   } else {
     const hasValidProjectType = PROJECT_LIST.includes(promptLowercase)
-    console.log(hasValidProjectType)
+
+    if (hasValidProjectType) {
+      const { installDeps } = await getPromptInstallDeps()
+
+      return { installDeps }
+    }
   }
+}
+
+const getPromptProjectType = async () => {
+  const projectType = await p.select({
+    message: 'Pick a project type.',
+    options: [
+      { value: 'react', label: 'React' },
+      { value: 'vue', label: 'Vue' },
+      { value: 'next', label: 'Next' },
+      { value: 'nuxt', label: 'Nuxt' },
+    ],
+  })
+
+  if (p.isCancel(projectType)) {
+    p.cancel(CANCELED_OP_MSG)
+    return process.exit(0)
+  }
+
+  return { projectType }
+}
+
+const getPromptInstallDeps = async () => {
+  const installDeps = await p.confirm({
+    message: 'Install needed depedencies ?',
+    initialValue: true,
+  })
+
+  if (p.isCancel(installDeps)) {
+    p.cancel(CANCELED_OP_MSG)
+    return process.exit(0)
+  }
+
+  return { installDeps }
 }
